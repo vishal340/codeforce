f0cf89513171b8b982e91386ef26e081d34397a7
vishal <vishaltripathy54@gmail.com>
2026-09-27 03:04:31 +0530

    2269 ABC

---
+++ 2269/B.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vll = vector<long long>;
+#define ai(x) array<int, (x)>
+#define al(x) array<ll, (x)>
+#define ap(x) array<bool, (x)>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define sz(x) static_cast<int>((x).size())
+#define all(x) (x).begin(), (x).end()
+#define fora(i, l, r, k) for (int i = (l); i < (r); i += (k))
+#define forb(i, r, l, k) for (int i = (r) - 1; i >= (l); i -= (k))
+#define rep(i, n) fora(i, 0, n, 1)
+#define per(i, n) forb(i, n, 0, 1)
+
+template <typename T> istream &operator>>(istream &in, vector<T> &vec) {
+  for (int i = 0; i < sz(vec); ++i)
+    in >> vec[i];
+  return in;
+}
+
+// cout-only: space-separated (do not use for debug / cerr).
+template <typename T> ostream &operator<<(ostream &out, const vector<T> &v) {
+  for (int i = 0; i < sz(v); ++i) {
+    if (i)
+      out << ' ';
+    out << v[i];
+  }
+  return out;
+}
+
+// cerr-only debug printers (ignore ostream<< for ranges/containers).
+template <typename T>
+concept DebugAtom = !is_array_v<remove_cvref_t<T>> && !requires(const T &t) {
+  t.begin();
+  t.end();
+} && requires(const T &x) { cerr << x; };
+template <DebugAtom T> void __print(const T &x) { cerr << x; }
+template <typename T, typename V> void __print(const pair<T, V> &x) {
+  cerr << '{';
+  __print(x.first);
+  cerr << ", ";
+  __print(x.second);
+  cerr << '}';
+}
+template <typename T>
+  requires(!DebugAtom<T>)
+void __print(const T &x) {
+  int f = 0;
+  cerr << '{';
+  for (auto &&e : x) {
+    cerr << (f++ ? ", " : "");
+    __print(e);
+  }
+  cerr << '}';
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v) {
+  __print(t);
+  if (sizeof...(v))
+    cerr << ", ";
+  _print(v...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...)                                                            \
+  cerr << "[" << #x << "] = [";                                                \
+  _print(x)
+#else
+#define debug(x...)
+#endif
+
+constexpr int MOD = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  vi a(n);
+  cin >> a;
+  auto l = [](int b) {
+    int r = 0;
+    while (b > 0) {
+      int t = b % 10;
+      b /= 10;
+      r += t * t;
+    }
+    return r;
+  };
+  ll ret = 0;
+  vi b(146);
+  for (int i : a) {
+    int t = i;
+    rep(j, 146) t = l(t);
+    ret += b[t]++;
+  }
+  cout << ret;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  if (cin >> t) {
+    while (t--) {
+      solve();
+      cout << '\n';
+    }
+  }
+  return 0;
+}
---
+++ 2269/C.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vll = vector<long long>;
+#define ai(x) array<int, (x)>
+#define al(x) array<ll, (x)>
+#define ap(x) array<bool, (x)>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define sz(x) static_cast<int>((x).size())
+#define all(x) (x).begin(), (x).end()
+#define fora(i, l, r, k) for (int i = (l); i < (r); i += (k))
+#define forb(i, r, l, k) for (int i = (r) - 1; i >= (l); i -= (k))
+#define rep(i, n) fora(i, 0, n, 1)
+#define per(i, n) forb(i, n, 0, 1)
+
+template <typename T> istream &operator>>(istream &in, vector<T> &vec) {
+  for (int i = 0; i < sz(vec); ++i)
+    in >> vec[i];
+  return in;
+}
+
+// cout-only: space-separated (do not use for debug / cerr).
+template <typename T> ostream &operator<<(ostream &out, const vector<T> &v) {
+  for (int i = 0; i < sz(v); ++i) {
+    if (i)
+      out << ' ';
+    out << v[i];
+  }
+  return out;
+}
+
+// cerr-only debug printers (ignore ostream<< for ranges/containers).
+template <typename T>
+concept DebugAtom = !is_array_v<remove_cvref_t<T>> && !requires(const T &t) {
+  t.begin();
+  t.end();
+} && requires(const T &x) { cerr << x; };
+template <DebugAtom T> void __print(const T &x) { cerr << x; }
+template <typename T, typename V> void __print(const pair<T, V> &x) {
+  cerr << '{';
+  __print(x.first);
+  cerr << ", ";
+  __print(x.second);
+  cerr << '}';
+}
+template <typename T>
+  requires(!DebugAtom<T>)
+void __print(const T &x) {
+  int f = 0;
+  cerr << '{';
+  for (auto &&e : x) {
+    cerr << (f++ ? ", " : "");
+    __print(e);
+  }
+  cerr << '}';
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v) {
+  __print(t);
+  if (sizeof...(v))
+    cerr << ", ";
+  _print(v...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...)                                                            \
+  cerr << "[" << #x << "] = [";                                                \
+  _print(x)
+#else
+#define debug(x...)
+#endif
+
+constexpr int MOD = 1e9 + 7;
+void solve() {
+  int n, k;
+  cin >> n >> k;
+
+  vi a(n);
+  cin >> a;
+
+  ll s = 0;
+
+  if (k == 1) {
+    for (int i = 0; i < n; i++)
+      s += a[i];
+    cout << s;
+    return;
+  }
+  int m = n;
+  while (k <= (m + 1) / 2) {
+    s += a[k - 1 + (n - m)];
+    m--;
+  }
+  k = m - k + 1;
+  rep(i, k) s += max(a[i], a[n - 1 - i]);
+  cout << s;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  if (cin >> t) {
+    while (t--) {
+      solve();
+      cout << '\n';
+    }
+  }
+  return 0;
+}
